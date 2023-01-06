8c328ce885ba0b19fa24e78004e85859fe857ec3
Whiletruend <bongage.benjamin@gmail.com>
2023-01-06 22:27:07 +0100

    (06/01/2023): Fix database.js by removing every timestamps columns ("createdAt", "updatedAt"). It causes some errors in INSERT, SELECT, queries, etc.

--- server/models/database.js
+++ server/models/database.js
@@ -8,7 +8,14 @@ const db_pass = ""; // Default: N/A
 const db_port = "3306"; // Default: 3306
 const db_name = "nodejs_api"; // Can be changed
 const db_url = process.env.DATABASE_URL || "mysql://" + db_user + ":" + db_pass + "@" + db_host + ":" + db_port + "/" + db_name; // Don't touch
-const db_conn = new Sequelize(db_url); // Init the connection with Sequelize using the db_url
+
+// Init the connection with Sequelize using the db_url
+// Removed the timestamps columns ("createdAt" & "updatedAt") to avoid any errors.
+const db_conn = new Sequelize(db_url, {
+    define: {
+        timestamps: false
+    }
+});
 
 // Check if the connection is done
 db_conn.authenticate().then(() => console.log("Sequelize:: DB Connected!"));
