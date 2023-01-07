494dd6a7407ca99f7e31b4072476def3bacc445f
Whiletruend <bongage.benjamin@gmail.com>
2023-01-07 01:54:52 +0100

    (07/01/2022): Fix for merge to main branch.

--- server/models/database.js
+++ server/models/database.js
@@ -4,7 +4,7 @@ const Sequelize = require("sequelize");
 // Variable(s)
 const db_host = "localhost"; // Default: localhost (127.0.0.1)
 const db_user = "root"; // Default: root
-const db_pass = "root"; // Default: N/A
+const db_pass = ""; // Default: N/A
 const db_port = "3306"; // Default: 3306
 const db_name = "nodejs_api"; // Can be changed
 const db_url = process.env.DATABASE_URL || "mysql://" + db_user + ":" + db_pass + "@" + db_host + ":" + db_port + "/" + db_name; // Don't touch
