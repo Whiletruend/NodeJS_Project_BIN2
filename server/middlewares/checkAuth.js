a72514faed9487b1f9b34f03e5b0aef7eed00414
Shxwnn <marien.moutima-backenga@edu.devinci.fr>
2023-01-07 14:50:06 +0100

    corrigé première condition (options).

--- server/middlewares/checkAuth.js
+++ server/middlewares/checkAuth.js
@@ -4,7 +4,7 @@ const SECRET = process.env.JWT_SECRET || "SECRET_password";
 module.exports = function(options = {}){
     return function(req, res, next){
         const Authorization = req.headers["authorization"];
-        if(!Authorization && noptions){
+        if(!Authorization && options.anonymous){
             return next();
         }
         if(!Authorization){
